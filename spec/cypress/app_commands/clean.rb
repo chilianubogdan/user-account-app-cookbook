b342ee90eae13766bb82c2550b46d21fc82057d5
Bogdanchil <chilianubogdan@gmail.com>
2020-05-09 10:12:45 +0200

    [Project: FE > Unit Tests > Vue Components + TS] added cypress-vue-unit-test
    aside: rails test env - logger should log > 1st attempt to fix

--- spec/cypress/app_commands/clean.rb
+++ spec/cypress/app_commands/clean.rb
@@ -1,3 +1,6 @@
+require "active_support/logger"
+require "active_support/tagged_logging"
+
 if defined?(DatabaseCleaner)
   # cleaning the database using database_cleaner
   DatabaseCleaner.strategy = :truncation
@@ -10,7 +13,9 @@ end
 
 pp "APP CLEANED BGY PP"
 
+logger.warn "APPCLEANED" # used by log_fail.rb
 Rails.logger.warn "APPCLEANED" # used by log_fail.rb
 # @todo: fix logging from command 
 # pp logger.inspect
 # pp Rails.inspect
+# 1st attempt failed again
